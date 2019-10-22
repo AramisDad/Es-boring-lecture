d41c6845af357ca4085dec354044913bde118ae0
Student <example@example.com>
2019-10-22 11:44:42 -0700

    add function to toggle box on paragraph tags

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $("p.first").click(function() {
+    $("p.first").toggleClass("withbox");
+  });
+  $("p.second").click(function() {
+    $("p.second").toggleClass("withbox");
+  });
+  $("p.two").click(function() {
+    $("p.two").toggleClass("withbox");
+  });
+});
